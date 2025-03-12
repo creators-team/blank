2af08826fffd43c05ee6c00bdf4f3c9c1c4fde09
Ali Osman Delismen <dlsmnosman@gmail.com>
2025-03-12 23:15:54 +0300

    🚀 setup project

---
+++ apps/docs/app/layout.tsx
@@ -0,0 +1,9 @@
+import type React from 'react';
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body>{children}</body>
+    </html>
+  );
+}
