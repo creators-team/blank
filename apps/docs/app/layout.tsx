62a37e38a77312a995f273252fb00edfdee27abf
Ali Osman Delismen <dlsmnosman@gmail.com>
2025-03-12 23:22:59 +0300

    ✨ Add Spinner component and update dependencies in pnpm-lock.yaml

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -1,9 +1,13 @@
+import { Spinner } from '@blankui/spinner';
 import type React from 'react';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>{children}</body>
+      <body>
+        {children}
+        <Spinner />
+      </body>
     </html>
   );
 }
