a2d8a0f69440d5cde4cafcd7ec6564e1c641009b
Ali Osman Delismen <dlsmnosman@gmail.com>
2025-03-15 01:57:37 +0300

    🔧 Update package configuration and build process: rename package, enhance build scripts, and improve tsup configuration for better output management.

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,9 +2,14 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['scripts/index.ts'],
-  outDir: 'dist',
-  sourcemap: false,
-  minify: true,
+  format: ['esm'],
   dts: true,
-  format: ['cjs', 'esm'],
+  clean: true,
+  shims: true,
+  banner: {
+    js: '#!/usr/bin/env node',
+  },
+  outDir: 'dist',
+  treeshake: true,
+  sourcemap: true,
 });
